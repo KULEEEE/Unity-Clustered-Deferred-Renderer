9649fbcbb0f307ad6368f80422966896c81bc232
LeeJaeYeon <kh0410le@naver.com>
2025-11-30 02:02:03 +0900

    First Unlit Renderer

--- Clustered-Deferred-Renderer/Assets/ClusteredRenderer/Runtime/ClusteredRenderPipeline.cs
+++ Clustered-Deferred-Renderer/Assets/ClusteredRenderer/Runtime/ClusteredRenderPipeline.cs
@@ -1,47 +1,125 @@
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Experimental.Rendering;
 
 public class ClusteredRenderPipeline : RenderPipeline
 {
-    private ClusteredRenderPipelineAsset pipelineAsset;
-    private ClusteredRenderer renderer;
-    private RenderGraph renderGraph;
-    
+    private ClusteredRenderPipelineAsset renderPipelineAsset;
+
+    private RenderGraph myRenderGraph;
 
     public ClusteredRenderPipeline(ClusteredRenderPipelineAsset asset)
     {
-        this.pipelineAsset = asset;
-
-        this.renderGraph = new RenderGraph("ClusteredRenderPipeline");
-        this.renderer = new ClusteredRenderer();
-    }
-
-    private static readonly ShaderTagId testShaderTagID = new ShaderTagId("TestUnlit");
-    private static readonly List<ShaderTagId> s_ShaderTagIds = new List<ShaderTagId>
-    {
-        testShaderTagID
-    };
-
-    // Definition of Data for RenderGraph Pass
-    private class OpaquePassData
-    {
-        public TextureHandle cameraColorTarget;
-    }
-    protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
-    {
-        foreach(var camera in cameras)
-        {
-            renderer.Render(context, camera, renderGraph);
-        }
-        
+        renderPipelineAsset = asset;
+        myRenderGraph = new RenderGraph("ClusteredRenderGraph");
     }
 
     protected override void Dispose(bool disposing)
     {
-        renderGraph.Cleanup();
-        renderGraph = null;
+        myRenderGraph?.Cleanup();
         base.Dispose(disposing);
+    }
+
+    protected override void Render(ScriptableRenderContext context, List<Camera> cameras)
+    {
+        foreach (var cam in cameras)
+        {
+            if (cam.enabled)
+            {
+                RenderCamera(context, cam);
+            }
+        }
+
+    }
+
+    class PassData
+    {
+        public TextureHandle cameraTarget;
+        public Material material;
+        public Color clearColor;
+    }
+
+    void RenderCamera(ScriptableRenderContext context, Camera cameraToRender)
+    {
+        context.SetupCameraProperties(cameraToRender);
+        var cmd = CommandBufferPool.Get("Example command buffer");
+
+        RenderGraphParameters rgParams = new RenderGraphParameters
+        {
+            commandBuffer = cmd,
+            scriptableRenderContext = context,
+            currentFrameIndex = Time.frameCount,
+        };
+
+        try
+        {
+            myRenderGraph.BeginRecording(rgParams);
+            using (var builder = myRenderGraph.AddRasterRenderPass<PassData>("Example render pass", out var passData))
+            {
+
+                RenderTargetInfo cameraTargetProperties = new RenderTargetInfo
+                {
+                    width = cameraToRender.pixelWidth,
+                    height = cameraToRender.pixelHeight,
+                    volumeDepth = 1,
+                    msaaSamples = 1,
+                    format = GraphicsFormat.R8G8B8A8_UNorm
+                };
+                passData.cameraTarget = myRenderGraph.ImportBackbuffer(BuiltinRenderTextureType.CameraTarget, cameraTargetProperties);
+
+                passData.material = new Material(Shader.Find("Examples/SimpleUnlitColor"));
+                passData.clearColor = cameraToRender.backgroundColor;
+
+                builder.SetRenderAttachment(passData.cameraTarget, 0, AccessFlags.Write);
+
+                // Make sure the render graph system keeps the render pass, even if it's not used in the final frame.
+                // Don't use this in production code, because it prevents the render graph system from removing the render pass if it's not needed.
+                builder.AllowPassCulling(false);
+
+                builder.SetRenderFunc(static (PassData passData, RasterGraphContext context) =>
+                {
+                    // Create a quad mesh
+                    Mesh mesh = new Mesh();
+
+                    Vector3[] vertices = new Vector3[4]
+                    {
+                        new Vector3(0, 0, 0),
+                        new Vector3(1f, 0, 0),
+                        new Vector3(0, 1f, 0),
+                        new Vector3(1f, 1f, 0)
+                    };
+                    mesh.vertices = vertices;
+
+                    int[] triangles = new int[6]
+                    {
+                        0, 2, 1,
+                        2, 3, 1
+                    };
+                    mesh.triangles = triangles;
+
+                    context.cmd.ClearRenderTarget(true, true, passData.clearColor);
+
+                    // Create a transformation matrix for the quad
+                    Matrix4x4 trs = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, 0), Vector3.one);
+
+                    // Draw the quad onto the camera target, using the shader and the source texture
+                    context.cmd.DrawMesh(mesh, trs, passData.material, 0);
+                });
+            }
+
+            myRenderGraph.EndRecordingAndExecute();
+        }
+        catch (Exception e)
+        {
+            if (myRenderGraph.ResetGraphAndLogException(e))
+                throw;
+        }
+
+        context.ExecuteCommandBuffer(cmd);
+        CommandBufferPool.Release(cmd);
+        context.Submit();
     }
 }
--- Clustered-Deferred-Renderer/Assets/ClusteredRenderer/Shaders/TestUnlit.shader
+++ Clustered-Deferred-Renderer/Assets/ClusteredRenderer/Shaders/TestUnlit.shader
@@ -1,20 +1,22 @@
-Shader "ClusteredRenderer/SimpleUnlit"
+// This defines a simple unlit Shader object that is compatible with a custom Scriptable Render Pipeline.
+// It applies a hardcoded color, and demonstrates the use of the LightMode Pass tag.
+// It is not compatible with SRP Batcher.
+
+Shader "Examples/SimpleUnlitColor"
 {
-    Properties
-    {
-        _BaseColor ("Base Color", Color) = (1, 1, 1, 1)
-    }
     SubShader
     {
-        Tags { "RenderType" = "Opaque", "LightMode" = "TestUnlit" }
-
         Pass
         {
+            // The value of the LightMode Pass tag must match the ShaderTagId in ScriptableRenderContext.DrawRenderers
+            Tags { "LightMode" = "ExampleLightModeTag"}
+
             HLSLPROGRAM
             #pragma vertex vert
             #pragma fragment frag
 
-            #include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+	float4x4 unity_MatrixVP;
+            float4x4 unity_ObjectToWorld;
 
             struct Attributes
             {
@@ -23,23 +25,20 @@ Shader "ClusteredRenderer/SimpleUnlit"
 
             struct Varyings
             {
-                float4 positionCS   : SV_POSITION;
+                float4 positionCS : SV_POSITION;
             };
 
-            CBUFFER_START(UnityPerMaterial)
-                float4 _BaseColor;
-            CBUFFER_END
-
-            Varyings vert(Attributes IN)
+            Varyings vert (Attributes IN)
             {
                 Varyings OUT;
-                OUT.positionCS = TransformObjectToHClip(IN.positionOS.xyz);
+                float4 worldPos = mul(unity_ObjectToWorld, IN.positionOS);
+                OUT.positionCS = mul(unity_MatrixVP, worldPos);
                 return OUT;
             }
 
-            float4 frag(Varyings IN) : SV_Target
+            float4 frag (Varyings IN) : SV_TARGET
             {
-                return _BaseColor;
+                return float4(0.5,1,0.5,1);
             }
             ENDHLSL
         }
